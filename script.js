3c244283d4763fa38f1babee8f18abae7a14e2b3
Ramie Mosely <ramiemosely5@gmail.com>
2021-04-01 14:08:59 -0400

    Begun framework for connect four game logic

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () =>
+{
+    const squares = document.querySelectorAll('.grid div')
+    const result = document.querySelector('#result')
+    const displayCurrentPlayer = document.querySelector('#currentPlayer')
+    let currentPlayer = 1
+
+    for(let i = 0; i < squares.length;i++)
+    {
+        squares[i].onclick = () => {
+            alert('You have clicked square ' + i)
+        }
+
+    }
+})
