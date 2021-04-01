b44fa6ee7136349d3657e650170120c9880e4dee
Ramie Mosely <ramiemosely5@gmail.com>
2021-04-01 14:30:08 -0400

    Added player checking, need to fix grid bug!

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 document.addEventListener('DOMContentLoaded', () =>
 {
+    //Grabbing elements from DOM and assigning them to variables
     const squares = document.querySelectorAll('.grid div')
     const result = document.querySelector('#result')
     const displayCurrentPlayer = document.querySelector('#currentPlayer')
     let currentPlayer = 1
 
-    for(let i = 0; i < squares.length;i++)
+    //Checking where player clicks 
+    for(let i = 0; i < squares.length; i++)
     {
         squares[i].onclick = () => {
-            alert('You have clicked square ' + i)
+            //if the sqaure below your current square is taken, you can go ontop of it
+            if (squares[i + 7].classList.contains('taken'))
+            {
+                if(currentPlayer == 1)
+                {
+                    squares[i].classList.add('taken')
+                    squares[i].classList.add('playerOne')
+                    currentPlayer = 2
+                    displayCurrentPlayer.innerHTML = currentPlayer
+                }
+
+                else if(currentPlayer == 2)
+                {
+                    squares[i].classList.add('taken')
+                    squares[i].classList.add('playerTwo')
+                    currentPlayer = 1
+                    displayCurrentPlayer.innerHTML = currentPlayer
+                }
+                else alert('INVALID OPERATION!')
+
+            }
         }
 
     }
